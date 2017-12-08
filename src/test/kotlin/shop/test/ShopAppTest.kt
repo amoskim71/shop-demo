31cee5bface9cae613e12cedaea4819975291203
Pawel Kapala <github@kapware.com>
2017-12-08 01:09:41 +0100

    Test cleanup with it and on

--- src/test/kotlin/shop/test/ShopAppTest.kt
+++ src/test/kotlin/shop/test/ShopAppTest.kt
@@ -16,31 +16,38 @@ class ShopAppTest {
     fun reset() = StockRepository.resetStock(0)
 
     @Test
-    fun `should support two kind of items with initial stock of 20 and 10`() = withTestApplication(Application::main) {
-        // when:
-        with (handleRequest(HttpMethod.Get, "/")) {
-            // then:
-            assertEquals(HttpStatusCode.OK, response.status())
-            assertTrue(response.content?.contains("Item A: 20 left")!!)
-            assertTrue(response.content?.contains("Item B: 10 left")!!)
+    fun `testing simple get`() = withTestApplication(Application::main) {
+        on ("first request to /") {
+            val result = handleRequest(HttpMethod.Get, "/")
+
+            it ("should return initial stock of 20 and 10") {
+                with (result) {
+                    assertEquals(HttpStatusCode.OK, response.status())
+                    assertTrue(response.content?.contains("Item A: 20 left")!!)
+                    assertTrue(response.content?.contains("Item B: 10 left")!!)
+                }
+            }
         }
     }
 
     @Test
-    fun `should allow buying stock of both items`() = withTestApplication(Application::main) {
-        // given:
+    fun `testing post`() = withTestApplication(Application::main) {
         StockRepository.resetStock(30)
 
-        // when:
-        with (handleRequest {
-            uri = "/stock"
-            method = HttpMethod.Post
-            body = "item_a=10&item_b=17"
-        }) {
-            // then:
-            assertEquals(HttpStatusCode.Created, response.status())
-            assertEquals(StockRepository.stockOf(Item.A), 20)
-            assertEquals(StockRepository.stockOf(Item.B), 13)
+        on("post to /stock") {
+            val result = handleRequest {
+                uri = "/stock"
+                method = HttpMethod.Post
+                body = "item_a=10&item_b=17"
+            }
+
+            it("should allow buying stock of both items") {
+                with(result) {
+                    assertEquals(HttpStatusCode.Created, response.status())
+                    assertEquals(StockRepository.stockOf(Item.A), 20)
+                    assertEquals(StockRepository.stockOf(Item.B), 13)
+                }
+            }
         }
     }
 }
--- src/test/kotlin/shop/test/StockRepositoryTest.kt
+++ src/test/kotlin/shop/test/StockRepositoryTest.kt
@@ -1,5 +1,7 @@
 package shop.test
 
+import io.ktor.server.testing.it
+import io.ktor.server.testing.on
 import org.junit.After
 import org.junit.Test
 import shop.Item
@@ -12,57 +14,68 @@ class StockRepositoryTest {
     fun clear() = StockRepository.resetStock(0)
 
     @Test
-    fun `should support setting initial stock`() {
-        // given:
+    fun `initial stock`() {
         assertEquals(0, StockRepository.stockOf(Item.A))
         assertEquals(0, StockRepository.stockOf(Item.B))
 
-        // when:
-        StockRepository.setStock(Item.A, 30)
-        StockRepository.setStock(Item.B, 90)
+        on ("setting initial stock") {
+            StockRepository.setStock(Item.A, 30)
+            StockRepository.setStock(Item.B, 90)
 
-        // then:
-        assertEquals(30, StockRepository.stockOf(Item.A))
-        assertEquals(90, StockRepository.stockOf(Item.B))
+            it ("should return set stock") {
+                assertEquals(30, StockRepository.stockOf(Item.A))
+                assertEquals(90, StockRepository.stockOf(Item.B))
+            }
+        }
     }
 
     @Test
-    fun `should support selling stock`() {
-        // given:
+    fun `selling stock`() {
         StockRepository.setStock(Item.A, 30)
         StockRepository.setStock(Item.B, 90)
         assertEquals(30, StockRepository.stockOf(Item.A))
         assertEquals(90, StockRepository.stockOf(Item.B))
 
-        // when:
-        StockRepository.sell(Item.A, 10)
-        StockRepository.sell(Item.B, 15)
+        on ("setting selling stock") {
+            StockRepository.sell(Item.A, 10)
+            StockRepository.sell(Item.B, 15)
 
-        // then:
-        assertEquals(20, StockRepository.stockOf(Item.A))
-        assertEquals(75, StockRepository.stockOf(Item.B))
+            it ("should decrease stock by sold amount") {
+                assertEquals(20, StockRepository.stockOf(Item.A))
+                assertEquals(75, StockRepository.stockOf(Item.B))
+            }
+        }
     }
 
     @Test
-    fun `should fail if there is not enough stock`() {
-        // given:
+    fun `not enough stock`() {
         StockRepository.setStock(Item.A, 0)
         StockRepository.setStock(Item.B, 10)
         assertEquals(0, StockRepository.stockOf(Item.A))
         assertEquals(10, StockRepository.stockOf(Item.B))
 
-        // when:
-        assertFailsWith(IllegalArgumentException::class) {
-            StockRepository.sell(Item.A, 1)
+        on ("selling if the stock is empty") {
+            it ("should fail with IllegalArgumentException") {
+                assertFailsWith(IllegalArgumentException::class) {
+                    StockRepository.sell(Item.A, 1)
+                }
+            }
+            it ("should remain empty stock") {
+                assertEquals(0, StockRepository.stockOf(Item.A))
+            }
         }
 
-        assertFailsWith(IllegalArgumentException::class) {
-            StockRepository.sell(Item.B, 31)
-        }
+        on ("selling if there is not enough stock") {
+            it ("should fail with IllegalArgumentException") {
+                assertFailsWith(IllegalArgumentException::class) {
+                    StockRepository.sell(Item.B, 31)
+                }
+            }
+            it ("should remain previous stock") {
+                assertEquals(10, StockRepository.stockOf(Item.B))
+            }
 
-        // then:
-        assertEquals(0, StockRepository.stockOf(Item.A))
-        assertEquals(10, StockRepository.stockOf(Item.B))
+        }
     }
 
 }
